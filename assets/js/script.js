a2381cd09d9db624feee00b9b4f677b9bd4256b9
Ruiran Xun <xun.ruiran@gmail.com>
2020-07-25 22:47:09 -0400

    today, I learned that particles.js exists

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,3 +25,97 @@ $(window).scroll(function() {
         }
     });
 });
+
+particlesJS('particles', {
+    "particles": {
+      "number": {
+        "value": 127,
+        "density": {
+          "enable": true,
+          "value_area": 700
+        }
+      },
+      "color": {
+        "value": "#ffffff"
+      },
+      "shape": {
+        "type": "polygon",
+        "stroke": {
+          "width": 2,
+          "color": "#ffffff80"
+        },
+        "polygon": {
+          "nb_sides": 6
+        },
+      },
+      "opacity": {
+        "value": 0.2,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 0.1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 5,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 40,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 150,
+        "color": "#ffffff",
+        "opacity": 0.4,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 2,
+        "direction": "none",
+        "random": false,
+        "straight": false,
+        "out_mode": "bounce",
+        "bounce": false,
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "bubble"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "repulse"
+        },
+        "resize": true
+      },
+      "modes": {
+        "bubble": {
+          "distance": 200,
+          "size": 7,
+          "duration": 2,
+          "opacity": 0.1,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 100,
+          "duration": 3,
+        }
+      }
+    },
+    "retina_detect": true
+  });
